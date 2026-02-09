b97351dc6ff0e956947f48883dd40ca55c701a00
Nguyen tung <tugnngw@gmail.com>
2026-02-09 11:28:45 +0700

    add shift configuration service

--- shift-service/src/main/java/com/franchiseproject/shiftservice/service/impl/ShiftConfigurationService.java
+++ shift-service/src/main/java/com/franchiseproject/shiftservice/service/impl/ShiftConfigurationService.java
@@ -1,4 +1,19 @@
 package com.franchiseproject.shiftservice.service.impl;
 
+
+import com.franchiseproject.shiftservice.model.ShiftConfiguration;
+import com.franchiseproject.shiftservice.repository.ShiftConfigurationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ShiftConfigurationService {
+    private final ShiftConfigurationRepository repository;
+
+    public List<ShiftConfiguration> getAllShifts() {
+        return repository.findAll();
+    }
 }
