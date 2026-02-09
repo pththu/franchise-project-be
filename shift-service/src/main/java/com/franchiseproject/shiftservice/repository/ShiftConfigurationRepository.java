e6b31cf9aaaa31d12a1e637191777e59cc58da97
Nguyen tung <tugnngw@gmail.com>
2026-02-09 11:26:53 +0700

    add shift configuration repository

--- shift-service/src/main/java/com/franchiseproject/shiftservice/repository/ShiftConfigurationRepository.java
+++ shift-service/src/main/java/com/franchiseproject/shiftservice/repository/ShiftConfigurationRepository.java
@@ -1,4 +1,9 @@
 package com.franchiseproject.shiftservice.repository;
 
-public class ShiftConfigurationRepository {
+import com.franchiseproject.shiftservice.model.ShiftConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ShiftConfigurationRepository extends JpaRepository<ShiftConfiguration, UUID> {
 }
