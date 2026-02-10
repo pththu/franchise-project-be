843f2bf9fcd247e8d1121f3c83f5c002f758c79e
DECIVAL\GIGABY <phamz12345@gmail.com>
2026-02-10 20:33:28 +0700

    feat:update delivery-service

---
+++ delivery-service/src/main/java/com/franchiseproject/deliveryservice/service/DeliveryService.java
@@ -0,0 +1,10 @@
+package com.franchiseproject.deliveryservice.service;
+
+import com.franchiseproject.deliveryservice.model.Delivery;
+import java.util.List;
+
+
+public interface DeliveryService {
+    List<Delivery> findAll();
+
+}
