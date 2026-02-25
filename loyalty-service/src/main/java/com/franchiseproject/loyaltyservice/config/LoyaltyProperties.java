9bb277931b074d4810dba31599eb6227e3c3ce01
Nguyễn Hữu Tài <tainh.ce190284@gmail.com>
2026-02-25 04:05:31 +0000

    feat(loyalty): Implement management for tier benefits. Create TierBenefit entity and repository

--- loyalty-service/src/main/java/com/franchiseproject/loyaltyservice/config/LoyaltyProperties.java
+++ loyalty-service/src/main/java/com/franchiseproject/loyaltyservice/config/LoyaltyProperties.java
@@ -4,12 +4,16 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "loyalty")
 public class LoyaltyProperties {
     private Rule rule = new Rule();
     private Tiers tiers = new Tiers();
+    private Benefits benefits = new Benefits();
 
     @Data
     public static class Rule {
@@ -22,5 +26,14 @@ public class LoyaltyProperties {
         private int gold = 1000;
         private int platinum = 2000;
         private int diamond = 3000;
+    }
+
+    @Data
+    public static class Benefits {
+        private List<String> bronze = new ArrayList<>();
+        private List<String> silver = new ArrayList<>();
+        private List<String> gold = new ArrayList<>();
+        private List<String> platinum = new ArrayList<>();
+        private List<String> diamond = new ArrayList<>();
     }
 }
