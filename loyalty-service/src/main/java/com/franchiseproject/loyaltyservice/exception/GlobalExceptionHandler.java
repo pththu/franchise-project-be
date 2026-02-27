0303649a89387dbb7b9aec9e7e4d31100e5da500
Nguyễn Hữu Tài <tainh.ce190284@gmail.com>
2026-02-27 01:55:35 +0000

    merge(loyalty): implement get available promotions API

--- loyalty-service/src/main/java/com/franchiseproject/loyaltyservice/exception/GlobalExceptionHandler.java
+++ loyalty-service/src/main/java/com/franchiseproject/loyaltyservice/exception/GlobalExceptionHandler.java
@@ -3,36 +3,40 @@ package com.franchiseproject.loyaltyservice.exception;
 import com.franchiseproject.loyaltyservice.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Bắt exception chung, chưa xác định
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<ApiResponse<Void>> handlingRuntimeException(Exception exception) {
-        ApiResponse<Void> response = ApiResponse.<Void>builder()
+    public ResponseEntity<ApiResponse> handlingRuntimeException(Exception exception) {
+        ApiResponse response = ApiResponse.builder()
                 .statusCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode())
                 .message(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage())
                 .build();
         return ResponseEntity.internalServerError().body(response);
     }
 
+    // Bắt App Exception
     @ExceptionHandler(value = AppException.class)
-    public ResponseEntity<ApiResponse<Void>> handlingAppException(AppException exception) {
+    public ResponseEntity<ApiResponse> handlingAppException(AppException exception) {
         ErrorCode errorCode = exception.getErrorCode();
-        ApiResponse<Void> response = ApiResponse.<Void>builder()
+        ApiResponse response = ApiResponse.builder()
                 .statusCode(errorCode.getCode())
                 .message(errorCode.getMessage())
                 .build();
 
+        // Nếu mã lỗi bắt đầu bằng số 404x thì trả HTTP 404, ngược lại trả 400
         int httpStatus = (String.valueOf(errorCode.getCode()).startsWith("404")) ? 404 : 400;
         return ResponseEntity.status(httpStatus).body(response);
     }
 
+    // Bắt exception vi phạm validation (@NotBlank, @NotNull...)
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<Void>> handlingMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    public ResponseEntity<ApiResponse> handlingMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         String errorKey = exception.getFieldError().getDefaultMessage();
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
         try {
@@ -40,7 +44,7 @@ public class GlobalExceptionHandler {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
-        ApiResponse<Void> response = ApiResponse.<Void>builder()
+        ApiResponse response = ApiResponse.builder()
                 .statusCode(errorCode.getCode())
                 .message(errorCode.getMessage())
                 .build();
@@ -48,10 +52,10 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(value = NoResourceFoundException.class)
-    public ResponseEntity<ApiResponse<Void>> handlingNoResourceFoundException (NoResourceFoundException exception) {
+    public ResponseEntity<ApiResponse> handlingNoResourceFoundException (NoResourceFoundException exception) {
         ErrorCode errorCode = ErrorCode.NOT_FOUND;
         return ResponseEntity.status(404)
-                .body(ApiResponse.<Void>builder()
+                .body(ApiResponse.builder()
                         .statusCode(errorCode.getCode())
                         .message(errorCode.getMessage())
                         .build());
