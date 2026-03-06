a973c482cd1feba06af1819d288d13b246933ba6
duy0205 <duyvo180205@gmail.com>
2026-03-06 16:18:42 +0700

    feat(delivery): add exception handler

---
+++ delivery-service/src/main/java/com/franchiseproject/deliveryservice/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.franchiseproject.deliveryservice.exception;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public enum ErrorCode {
+    VALIDATION_FAILED(400, "Dữ liệu không hợp lệ", HttpStatus.BAD_REQUEST);
+    int code;
+    String message;
+    HttpStatus httpStatus;
+
+    ErrorCode(int code, String message, HttpStatus httpStatus) {
+        this.code = code;
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+}
