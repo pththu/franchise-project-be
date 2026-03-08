774f12abd18391b644d3c7674f60d1fa5d40c54d
Bảo Trâm <baotrammari2003@gmail.com>
2026-03-09 03:53:05 +0700

    feat: set cookie when login, add option login with email or username

--- identity-access-service/src/main/java/com/franchiseproject/identityaccessservice/repository/UserRepository.java
+++ identity-access-service/src/main/java/com/franchiseproject/identityaccessservice/repository/UserRepository.java
@@ -13,5 +13,5 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
-//    Optional<User> findByCognitoSub(String cognitoSub);
+    Optional<User> findByUsernameOrEmail(String username, String email);
 }
