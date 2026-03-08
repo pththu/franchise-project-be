99fc8d3993cd2b440dd4122b5fc56b25fe8deb52
Bảo Trâm <baotrammari2003@gmail.com>
2026-03-09 05:38:08 +0700

    feat: register account system

--- identity-access-service/src/main/java/com/franchiseproject/identityaccessservice/dto/request/UserRegisterRequest.java
+++ identity-access-service/src/main/java/com/franchiseproject/identityaccessservice/dto/request/UserRegisterRequest.java
@@ -5,12 +5,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserRegisterRequest {
 
