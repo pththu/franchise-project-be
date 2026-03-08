aaf0efdd89319a7a70f0086c1a4309ec8e3ebe60
HMinhAI <lehoangminh.ai.tech@gmail.com>
2026-03-08 19:46:03 +0700

    feat: thêm và cấu hình api dịch các văn bản sang ngôn ngữ khác trong 'translate.py' và 'service.py', thêm xử lý lỗi khi không đủ các trường để tạo vector trong 'semantic_search.py'

---
+++ AI-service/translate.py
@@ -0,0 +1,38 @@
+from google import genai
+import time
+
+
+def translater(text_to_translate: list, target_language: str):
+    client = genai.Client(api_key='AIzaSyCx_YyRCxZMPrjj8vol_a4rviST19URW_o')
+
+    system_prompt = (
+        "Bạn là một người phiên dịch chuyên nghiệp. "
+        "Nhiệm vụ: Dịch văn bản tiếng việt sang các ngôn ngữ khác. "
+        "Quy tắc: Giữ đúng thứ tự, không giải thích, không thêm lời chào, ngăn cách giữa các câu là dấu ',' ."
+        "chỉ trả về nội dung đã dịch."
+    )
+
+    text = ", ".join(text_to_translate)
+
+    start = time.time()
+
+    response = client.models.generate_content(
+        model="gemini-3.1-flash-lite-preview", 
+        contents= f"{text} dịch tất cả sang ngôn ngữ {target_language}",
+        config={
+            "system_instruction": system_prompt,
+            "temperature": 0.0, 
+        }
+    )
+
+    response_api = response.text.split(',')
+
+    end = time.time()
+
+    return {"time response": end - start, "text": response_api}
+
+
+if __name__ == "__main__":
+    response = translater(["tôi tên là minh", "tôi là sinh viên trường đại học fpt"], 'jp')
+    print(response['time response'])
+    print(response['text'])
