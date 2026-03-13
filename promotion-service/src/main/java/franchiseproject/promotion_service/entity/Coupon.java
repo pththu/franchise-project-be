cedd0916557a9550bf2cb98cc5ac3e179b844314
trantiendat66 <dattt.ce190362@gmail.com>
2026-03-13 08:36:14 +0700

    fix(promotion-service): prevent circular reference between Promotion and Coupon

--- promotion-service/src/main/java/franchiseproject/promotion_service/entity/Coupon.java
+++ promotion-service/src/main/java/franchiseproject/promotion_service/entity/Coupon.java
@@ -6,7 +6,7 @@ import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.UuidGenerator;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.Instant;
 import java.util.UUID;
 
@@ -36,6 +36,7 @@ public class Coupon {
 
     @ManyToOne
     @JoinColumn(name = "promotion_id")
+    @JsonIgnore
     Promotion promotion;
 
     @CreationTimestamp
