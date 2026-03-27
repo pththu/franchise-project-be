ff4812bc415969e3c5ec6bda30aa57a47bd40875
Hà Minh Mẫn <hmman.ce190070@gmail.com>
2026-03-27 04:12:46 +0000

    refactor(customer-service): replace Customer with User and integrate Loyalty Service

--- customer-service/src/main/java/com/franchiseproject/customerservice/dto/response/CustomerTierResponse.java
+++ customer-service/src/main/java/com/franchiseproject/customerservice/dto/response/CustomerTierResponse.java
@@ -3,14 +3,17 @@ package com.franchiseproject.customerservice.dto.response;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import java.util.List;
+import java.util.UUID;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class CustomerDetailResponse {
-    CustomerFranchiseResponse customer;
-    List<LoyaltyInfoResponse> loyaltyInfos;
+public class CustomerTierResponse {
+    UUID customerId;
+    UUID franchiseId;
+    String currentTier;
+    Integer currentPoints;
+    Integer totalPoints;
 }
